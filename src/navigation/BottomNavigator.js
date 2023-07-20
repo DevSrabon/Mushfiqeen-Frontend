912f00d7be070e19ffb063a2c294b6a10253bc9e
Mahbub Morshed <mahbub.bogra30@gmail.com>
2023-07-20 13:17:36 +0600

    bottomNav

--- src/navigation/BottomNavigator.js
+++ src/navigation/BottomNavigator.js
@@ -12,107 +12,113 @@ const screenOptions = {
   tabBarStyle: {
     position: "absolute",
     height: 60,
-    background: "#fff",
+    background: "#1d2226",
   },
 };
 export default function BottomNavigator() {
   return (
-    <>
-      <Tab.Navigator
-        backBehavior="Main"
-        initialRouteName="Main"
-        screenOptions={screenOptions}
-      >
-        <Tab.Screen
-          name="home"
-          component={Home}
-          options={{
-            title: "Home",
-            tabBarIcon: ({ focused }) => {
-              return (
+    <Tab.Navigator
+      backBehavior="Main"
+      initialRouteName="Main"
+      screenOptions={{
+        tabBarInactiveTintColor: colors.lightGray,
+        tabBarActiveTintColor: colors.white,
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: colors.bg,
+        },
+        tabBarLabelStyle: {
+          bottom: 5,
+          fontFamily: "SemiBold",
+        },
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused, color }) => (
+            <Entypo
+              name={"home"}
+              size={24}
+              color={focused ? colors.white : colors.lightGray}
+            />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="post"
+        component={Post}
+        options={{
+          title: "Post",
+          tabBarIcon: ({ focused, color }) => (
+            <MaterialIcons
+              name={"post-add"}
+              size={24}
+              color={focused ? colors.white : colors.lightGray}
+            />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="bayan"
+        component={Bayan}
+        options={{
+          title: "Bayan",
+          tabBarIcon: ({ focused, color }) => (
+            <Entypo
+              name={"sound"}
+              size={24}
+              color={focused ? colors.white : colors.lightGray}
+            />
+          ),
+        }}
+      />
+      {/* <Tab.Screen
+        name="bayan"
+        component={Bayan}
+        options={{
+          title: "Bayan",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View>
                 <View
-                  style={{ alignItems: "center", justifyContent: "center" }}
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: colors.bg,
+                    width: Platform.OS == "ios" ? 50 : 60,
+                    height: Platform.OS == "ios" ? 50 : 60,
+                    top: Platform.OS == "ios" ? -10 : -20,
+                    borderRadius: Platform.OS == "ios" ? 25 : 30,
+                  }}
                 >
-                  <Entypo
-                    name="home"
-                    size={24}
-                    color={focused ? `${colors.bg}` : "#111"}
-                  />
-                  <Text>HOME</Text>
+                  <Entypo name="sound" size={24} color="#fff" />
                 </View>
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="post"
-          component={Post}
-          options={{
-            title: "Post",
-            tabBarIcon: ({ focused }) => {
-              return (
-                <View
-                  style={{ alignItems: "center", justifyContent: "center" }}
-                >
-                  <MaterialIcons
-                    name="post-add"
-                    size={24}
-                    color={focused ? `${colors.bg}` : "#111"}
-                  />
-                  <Text style={{ fonSize: 12, color: "#16247d" }}>Post</Text>
-                </View>
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="bayan"
-          component={Bayan}
-          options={{
-            title: "Bayan",
-            tabBarIcon: ({ focused }) => {
-              return (
-                <View>
-                  <View
-                    style={{
-                      alignItems: "center",
-                      justifyContent: "center",
-                      backgroundColor: colors.bg,
-                      width: Platform.OS == "ios" ? 50 : 60,
-                      height: Platform.OS == "ios" ? 50 : 60,
-                      top: Platform.OS == "ios" ? -10 : -20,
-                      borderRadius: Platform.OS == "ios" ? 25 : 30,
-                    }}
-                  >
-                    <Entypo name="sound" size={24} color="#fff" />
-                  </View>
-                </View>
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="chat"
-          component={Chat}
-          options={{
-            title: "Chat",
-            tabBarIcon: ({ focused }) => {
-              return (
-                <View
-                  style={{ alignItems: "center", justifyContent: "center" }}
-                >
-                  <MaterialIcons
-                    name="chat"
-                    size={24}
-                    color={focused ? `${colors.bg}` : "#111"}
-                  />
-                  <Text style={{ fonSize: 12, color: "#16247d" }}>Chat</Text>
-                </View>
-              );
-            },
-          }}
-        />
-      </Tab.Navigator>
-    </>
+              </View>
+            );
+          },
+        }}
+      /> */}
+
+      <Tab.Screen
+        name="chat"
+        component={Chat}
+        options={{
+          title: "Chat",
+          tabBarIcon: ({ focused, color }) => (
+            <MaterialIcons
+              name={"chat"}
+              size={24}
+              color={focused ? colors.white : colors.lightGray}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
